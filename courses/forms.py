603e48dcf6a9209eaed6e166ee40f572ee6dea94
Peter <12ptr12@pm.me>
2023-09-02 12:50:14 +0200

    Module Update View

--- courses/forms.py
+++ courses/forms.py
@@ -1,8 +1,19 @@
+from django import forms
+from django.forms.models import inlineformset_factory
 from django.forms import ModelForm
-from .models import Subject
+from .models import Course, Module, Subject
 
 class SubjectForm(ModelForm):
 
     class Meta:
         model = Subject
         fields = ['title', 'slug']
+
+ModuleFormSet = inlineformset_factory(Course, 
+                                      Module, 
+                                      fields=[
+                                          'title',
+                                          'description',
+                                      ],
+                                      extra=2,
+                                      can_delete=True)
