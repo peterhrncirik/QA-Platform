089f8ccdfa9806eda3a9cdbc2452806447682726
Peter <12ptr12@pm.me>
2023-09-14 13:26:23 +0200

    Added new Course views and functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # QA-Platform
+
+E-Learning platform with the goal of helping automation testers in improving their skills.
--- tests/courses/test_courses_views.py
+++ tests/courses/test_courses_views.py
@@ -24,3 +24,26 @@ def test_courses_view_redirect_not_logged_in(client):
     response = client.get(url)
     assert response.status_code == 302
 
+# @pytest.mark.xfail(reason="URLs not implemented yet")
+@pytest.mark.django_db
+def test_module_create_content_view(admin_client):
+
+    """
+        Tests whether Logged-in User with Permissions Can Create a Module
+    """
+
+    url = reverse('module_content_create', kwargs={'module_id': 6, 'model_name': 'image'})
+    response = admin_client.get(url)
+    assert response.status_code == 200
+
+@pytest.mark.xfail(reason="URLs not implemented yet")
+@pytest.mark.django_db
+def test_module_update_content_view(client):
+
+    """
+        Tests whether not Logged-in User is Redirected when Trying to Create new Course
+    """
+
+    url = reverse('module_content_create', kwargs={'module_id': 1, 'model_name': 'content'})
+    response = client.get(url)
+    assert response.status_code == 302
