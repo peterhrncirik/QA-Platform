c8333a26836338f45bd8535f2c84415dc07aecb3
Peter <12ptr12@pm.me>
2023-09-15 10:11:35 +0200

    Added Courses list view & Course detail view

--- courses/views.py
+++ courses/views.py
@@ -7,12 +7,16 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.urls import reverse_lazy
 from django.forms.models import modelform_factory
 from django.apps import apps
+from django.db.models import Count
+from django.views.generic.detail import DetailView
+
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 from .forms import ModuleFormSet
 
-# Create your views here.
+# Instructor Views
+
 class ManageCourseListView(ListView):
     model = Course
     template_name = 'courses/manage/course/list.html'
@@ -159,3 +163,29 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         
         return self.render_json_response({'saved': 'OK'})
         
+# General Courses Views
+
+class CourseListView(TemplateResponseMixin, View):
+
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        
+        return self.render_to_response({
+            'subjects': subjects,
+            'subject': subject,
+            'courses': courses,
+        })
+    
+class CourseDetailView(DetailView):
+
+    model = Course
+    template_name = 'courses/course/detail.html'
--- django_project/urls.py
+++ django_project/urls.py
@@ -4,17 +4,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from courses.views import CourseListView
+
 urlpatterns = [
-    path(os.environ.get("ADMIN_URL"), admin.site.urls),
-    path("accounts/", include("allauth.urls")),
+    path(os.environ.get('ADMIN_URL'), admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('course/', include('courses.urls')),
-    path("", include("pages.urls")),
+    path('courses/', CourseListView.as_view(), name='courses_list'),
+    path('', include('pages.urls')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
+        path('__debug__/', include(debug_toolbar.urls)),
         # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     ] + urlpatterns
