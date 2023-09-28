bc63658ab71386ec7e9d31e5116f7f44c5590782
Peter <12ptr12@pm.me>
2023-09-28 12:40:10 +0200

    Added School filter for user

--- accounts/templatetags/group.py
+++ accounts/templatetags/group.py
@@ -1,6 +1,16 @@
 from django import template
 register = template.Library()
 
+from courses.models import School
+
 @register.filter(name='has_group')
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+@register.filter(name='school')
+def school(user):
+
+    if user.is_instructor:
+        return School.objects.get(instructors=user)
+    elif user.is_student:
+        return School.objects.get(students=user)
